65338918b0f2aac069b990528398e473bcff8f44
hesam <hesam.enayatfard@gmail.com>
2023-02-26 01:26:37 +0330

    add make object to tag function

---
+++ src/constants/data.ts
@@ -0,0 +1,45 @@
+export const dataMap = [
+  {
+    tag: "div",
+    class: "flex flex-col space-x-3 bg-red-200 p-10",
+    children: [
+      {
+        tag: "div",
+        class: "flex flex-col space-y-10 items-center bg-pink-200 space-x-5",
+        body: "fuck sdljaslj",
+        children: [
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+          {
+            tag: "div",
+            class: "bg-red-500 text-4xl",
+            body: "lsdkjaljs",
+          },
+        ],
+      },
+    ],
+  },
+];
